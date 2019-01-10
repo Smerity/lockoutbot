5c5d5a5fa13a31842451df03551bad64aeb3dcc8
Stephen Merity <smerity@smerity.com>
2019-01-10 20:30:38 +1100

    Setting in channel response

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/slack/lockedout', methods=['GET', 'POST'])
 def lockedout():
-    return jsonify({"text": "Sending Tim to help you", "in_channel": True})
+    return jsonify({"text": "Sending Tim to help you", "response_type": "in_channel"})
 
 if __name__ == '__main__':
     app.run(debug=True)
