0dbbe67d45e2f94b6eca10c47f03ecb366afb8e9
Stephen Merity <smerity@smerity.com>
2019-01-10 20:28:19 +1100

    Added jsonify and a more complex response

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/slack/lockedout', methods=['GET', 'POST'])
 def lockedout():
-    return 'Sending Tim to help you'
+    return jsonify({"text": "Sending Tim to help you", "in_channel": True})
 
 if __name__ == '__main__':
     app.run(debug=True)
