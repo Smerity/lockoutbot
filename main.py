c06d3c7d6219546652394add07fa07b8e9cb6aaf
Stephen Merity <smerity@smerity.com>
2019-01-10 20:33:55 +1100

    Markdown example

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/slack/lockedout', methods=['GET', 'POST'])
 def lockedout():
-    return jsonify({"text": "Sending Tim to help you", "response_type": "in_channel"})
+    return jsonify({"text": "Sending *Tim* to ~murder~ _help_ `you`", "mrkdwn": True})
 
 if __name__ == '__main__':
     app.run(debug=True)
