d0926239dc19d8b09c683ee68eea8d7cc0aaa1d9
Stephen Merity <smerity@smerity.com>
2019-01-11 09:19:23 +1100

    Adding an in channel response for Tim

--- main.py
+++ main.py
@@ -6,34 +6,7 @@ app = Flask(__name__)
 
 @app.route('/slack/lockedout', methods=['GET', 'POST'])
 def lockedout():
-    response = {
-        "text": "Which tutor would you like?",
-        "attachments": [
-            {
-                "text": "Pick a tutor",
-                "callback_id": "tutor_selection",
-                "actions": [
-                    {
-                        "name": "tutor",
-                        "type": "select",
-                        "data_source": "users",
-                    }
-                ]
-            }
-        ]
-    }
-    return jsonify(response)
-
-@app.route('/slack/interaction', methods=['GET', 'POST'])
-def slack_interaction():
-    payload = json.loads(request.values['payload'])
-    pprint.pprint(payload)
-
-    action = payload['actions'][0]
-    option = action['selected_options'][0]
-    value = option['value']
-    
-    return f"Sending <@{value}> to save you"
+    return jsonify({"text": "Sending Tim to help you", "response_type": "in_channel"})
 
 if __name__ == '__main__':
     app.run(debug=True)
