e75039bcb5cc7a68825bc96223498ee6bb03c290
Stephen Merity <smerity@smerity.com>
2019-01-10 20:40:36 +1100

    Attachments for sending Tim as a tutor when locked out

--- main.py
+++ main.py
@@ -4,7 +4,16 @@ app = Flask(__name__)
 
 @app.route('/slack/lockedout', methods=['GET', 'POST'])
 def lockedout():
-    return jsonify({"text": "Sending *Tim* to ~murder~ _help_ `you`", "mrkdwn": True})
+    response = {
+        "text": "Sending a tutor to help you",
+        "attachments": [
+            {
+                "title": "Tutor",     
+                "text": "Tim"
+            }
+        ]
+    }
+    return jsonify(response)
 
 if __name__ == '__main__':
     app.run(debug=True)
