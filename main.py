3ac557a88a4d54bb8359ce7cef545a8a0b792a4c
Stephen Merity <smerity@smerity.com>
2019-01-11 09:46:18 +1100

    Select a tutor from a dropdown list

--- main.py
+++ main.py
@@ -6,7 +6,24 @@ app = Flask(__name__)
 
 @app.route('/slack/lockedout', methods=['GET', 'POST'])
 def lockedout():
-    return jsonify({"text": "Sending Tim to help you", "response_type": "in_channel"})
+    return jsonify({
+        "text": "Which tutor would you like?",
+        "response_type": "in_channel",
+        "attachments": [
+            {
+                "callback_id": "tutor_selection",
+                "text": "Pick a tutor",
+                "fallback": "You didn’t pick a tutor.",
+                "actions": [
+                    {
+                        "name": "tutor",
+                        "type": "select",
+                        "data_source": "users"
+                    }
+                ]
+            }
+        ]
+    })
 
 if __name__ == '__main__':
     app.run(debug=True)
